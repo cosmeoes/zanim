1d8575d870e9496b6f2d67c18650e4a40b328a8d
Cosme Escobedo <comeoes@gmail.com>
2025-11-02 18:09:06 -0600

    lines with width

--- src/drawables/drawable_types.zig
+++ src/drawables/drawable_types.zig
@@ -18,15 +18,24 @@ pub fn generateVerticesUsingType(drawable: *Drawable) !void {
 }
 
 pub const Line = struct {
+    const DEFAULT_WIDTH: f32 = 0.01;
     base: Drawable,
+    // Do not modify directly, use helper methods
+    startPos: Vec3,
+    endPos: Vec3,
+    width: f32,
 
     pub fn init(allocator: std.mem.Allocator, startPos: Vec3, endPos: Vec3, color: Vec3) error{OutOfMemory}!Line {
-        const vertices = [_]Vec3{ startPos, endPos };
-        var line = Line{
-            .base = try Drawable.init(allocator, .LineSegments, .Line, &vertices),
-        };
-        line.base.setColor(color);
+        const vertices = lineToVertices(startPos, endPos, DEFAULT_WIDTH);
 
+        var line = Line{
+            .base = try Drawable.init(allocator, .TriangleMesh, .Line, &vertices),
+            .startPos = startPos,
+            .endPos = endPos,
+            .width = DEFAULT_WIDTH,
+        };
+
+        line.base.setColor(color);
         return line;
     }
 
@@ -35,26 +44,55 @@ pub const Line = struct {
     }
 
     pub fn setStart(self: *Line, newValue: Vec3) void {
-        self.base.vertices.items[0] = newValue;
-    }
-
-    pub fn start(self: *Line) Vec3 {
-        return self.base.vertices.items[0];
+        self.startPos = newValue;
+        self.updateVertices();
     }
 
     pub fn setEnd(self: *Line, newValue: Vec3) void {
-        self.base.vertices.items[1] = newValue;
+        self.endPos = newValue;
+        self.updateVertices();
     }
 
-    pub fn end(self: *Line) Vec3 {
-        return self.base.vertices.items[1];
+    pub fn setWidth(self: *Line, width: f32) void {
+        self.width = width;
+        self.updateVertices();
+    }
+
+    fn updateVertices(self: *Line) void {
+        lineToVertices(self.startPos, self.endPos, self.width);
+    }
+
+    // Converts the two points that define a line into the
+    // four points of a rectangle to draw a line width width.
+    fn lineToVertices(startPos: Vec3, endPos: Vec3, width: f32) [4]Vec3 {
+        var normal = Vec3.new(-1 * (endPos.y() - startPos.y()), endPos.x() - startPos.x(), 0).norm();
+        normal = normal.scale(width / 2);
+
+        const vertices = [_]Vec3{
+            startPos.add(normal), startPos.sub(normal), endPos.sub(normal), endPos.add(normal),
+        };
+
+        return vertices;
     }
 
     pub fn generateVertices(drawable: *Drawable) !void {
-        try drawable.appendVec3(drawable.vertices.items[0]);
+        const topLeft = drawable.vertices.items[0];
+        const bottomLeft = drawable.vertices.items[1];
+        const bottomRight = drawable.vertices.items[2];
+        const topRight = drawable.vertices.items[3];
+
+        try drawable.appendVec3(topLeft);
+        try drawable.appendVec3(drawable.color);
+        try drawable.appendVec3(bottomLeft);
+        try drawable.appendVec3(drawable.color);
+        try drawable.appendVec3(topRight);
         try drawable.appendVec3(drawable.color);
 
-        try drawable.appendVec3(drawable.vertices.items[1]);
+        try drawable.appendVec3(bottomLeft);
+        try drawable.appendVec3(drawable.color);
+        try drawable.appendVec3(bottomRight);
+        try drawable.appendVec3(drawable.color);
+        try drawable.appendVec3(topRight);
         try drawable.appendVec3(drawable.color);
     }
 };
